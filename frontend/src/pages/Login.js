1d8baf8324ce2a1b2977a5f24f037ccb9bd9949f
JS <67136819+skyjune0824@users.noreply.github.com>
2023-03-21 20:35:55 -0400

    Login background

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import '../App.css'
+import Bgimg from './BG.png';
+import styled from 'styled-components';
 
 function Login(){
     return(
-        <div>Random thing</div>
+        <div style={{
+            backgroundImage: `url(${Bgimg})`,
+            backgroundRepeat: 'repeat',
+            backgroundPosition: 'center',
+            width: '100vw',
+            height: '100vh',
+            textAlign: 'center',
+            alignContent: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <div style={{backgroundColor: 'white', width: '500px', justifyContent: 'center', alignItems: 'center'}}>
+                Nothing
+            </div>
+        </div>
     );
 }
 
 export default Login;
-
-
-
