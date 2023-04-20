5ef69c02bbec219bcd733217a8faab308b622f7e
Charles Chae <charleschae12@gmail.com>
2023-04-20 14:21:03 -0400

    Changed Orgs Manage page

--- frontend/src/components/GreekLifeView.js
+++ frontend/src/components/GreekLifeView.js
@@ -1,13 +1,15 @@
 import GreekLifeItem from './GreekLife'
+import React, {useState, useEffect} from 'react';
 
-function GreekLifeView(props) {
+function GreekLifeView(props){
+
   return (
     <div>
       <ul>
         {props.greekLifeList.map(GreekLife => <GreekLifeItem GreekLife={GreekLife} />)}
       </ul>
     </div>
-  )
+  );
 }
 
 export default GreekLifeView
